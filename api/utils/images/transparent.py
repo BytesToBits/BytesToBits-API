9d2109663d0f6ff5fc63aa55eb8d0a2a9fa44e5a
Nemika <nemika@bytestobits.dev>
2022-03-19 17:02:24 +0200

    Added background option to transparency

--- api/utils/images/transparent.py
+++ api/utils/images/transparent.py
@@ -1,10 +1,10 @@
 from math import isclose
-from PIL import Image, ImageColor
+from PIL import Image, ImageColor, ImageOps
 from io import BytesIO
 
 import requests
 
-async def clear_background(image, color="#ffffff", strength=20):
+async def clear_background(image, color="#ffffff", strength=20, background=None):
     image = Image.open(BytesIO(requests.get(image).content)).convert('RGBA')
     image_data = image.load()
 
@@ -15,6 +15,12 @@ async def clear_background(image, color="#ffffff", strength=20):
         for x in range(image.width):
             if isclose(sum(image_data[x,y]), sum(color), abs_tol=abs(int(strength))):
                 image_data[x,y] = replacement
+
+    if background:
+        background = Image.open(BytesIO(requests.get(background).content)).convert('RGBA')
+        background = ImageOps.fit(background, image.size, centering=(.5,.5))
+        background.paste(image, mask=image)
+        image = background
 
 
     img_bytes = BytesIO()
